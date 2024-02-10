a3f8c12f27df9c9c7184294fe8e0c2ef3ef83f57
Fadi Adib <fadi.adib@gmail.com>
2024-02-09 16:29:16 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,52 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { findPost, getPosts, editPost, deletePost, addPost } from "./posts.js";
+
+const port = 3000;
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+// Blog Home Page
+app.get("/", (req, res) => {
+  res.render("index.ejs", {
+    posts: getPosts(),
+  });
+});
+
+// Create a new blog
+app.get("/post", (req, res) => {
+  res.render("post.ejs");
+});
+app.post("/post", (req, res) => {
+  addPost(req.body.title, req.body.body);
+  res.redirect("/");
+});
+
+// Delete a post
+app.post("/delete/:id", (req, res) => {
+  const id = req.params.id;
+  deletePost(id);
+  res.redirect("/");
+});
+
+// Show a post
+app.get("/:id", (req, res) => {
+  const id = req.params.id;
+  res.render("post.ejs", {
+    post: findPost(id),
+  });
+});
+
+// Edit a post
+app.post("/:id", (req, res) => {
+  const id = req.params.id;
+  editPost(id, req.body.title, req.body.body);
+  res.redirect("/");
+});
+
+// Start server
+app.listen(port, () => {
+  console.log(`Server started at port ${port}`);
+});
---
+++ posts.js
@@ -0,0 +1,82 @@
+import fs from "fs";
+
+let posts = [];
+
+function getPostIdx(id) {
+  return posts.findIndex((element) => {
+    return element.id == id;
+  });
+}
+
+function savePosts() {
+  var data = JSON.stringify(posts);
+
+  fs.writeFile("./data.txt", data, (err) => {
+    if (err) {
+      throw err;
+    } else {
+      console.log("Data updated to disk");
+    }
+  });
+}
+
+function loadPosts() {
+  fs.readFile("./data.txt", { encoding: "utf8" }, (error, data) => {
+    if (error) {
+      throw error;
+    } else {
+      var result = JSON.parse(data);
+      posts = result;
+      console.log("Data read from disk");
+    }
+  });
+}
+
+export function getPosts() {
+  return posts;
+}
+
+export function findPost(id) {
+  const idx = getPostIdx(id);
+  if (idx >= 0) {
+    return posts[idx];
+  }
+}
+
+export function addPost(title, body) {
+  if (title.length && body.length) {
+    posts.push({
+      id: Math.random(),
+      title: title,
+      body: body,
+      date: new Date().toDateString(),
+    });
+
+    savePosts();
+  }
+}
+
+export function deletePost(id) {
+  const idx = getPostIdx(id);
+  if (idx >= 0) {
+    posts.splice(idx, 1);
+
+    savePosts();
+  }
+}
+
+export function editPost(id, title, body) {
+  const idx = getPostIdx(id);
+  if (idx >= 0) {
+    posts[idx] = {
+      id: id,
+      title: title,
+      body: body,
+      date: new Date().toDateString(),
+    };
+
+    savePosts();
+  }
+}
+
+loadPosts();
